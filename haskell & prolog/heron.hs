2a29a262a29c27e8a9736c06bda5a080dfd7a395
a-egealopez <alej.egealopez@gmail.com>
2025-12-08 19:53:08 +0100

    Versión de Herón DEFINITIVA

--- haskell & prolog/heron.hs
+++ haskell & prolog/heron.hs
@@ -1,3 +1,30 @@
+---------------------------- VERSIÓN DEFINITIVA ----------------------------
+
+import Data.List (sort)
+
+heron :: (Ord a, Fractional a) => a -> a -> a -> Maybe a
+heron a b c
+  | not $ isTriangle a b c = Nothing
+  | otherwise              = Just . newton f df $ x0
+  where
+    s = (a+b+c)/2
+    t = s*(s-a)*(s-b)*(s-c)
+    f x = x*x-t
+    df x = 2*x
+    x0 = max 1 $ t/2
+
+    isTriangle :: (Ord a, Num a) => a -> a -> a -> Bool 
+    isTriangle a b c = x>0 && x+y>z
+      where [x,y,z] = sort [a,b,c]
+
+    newton :: (Ord a, Fractional a) => (a -> a) -> (a -> a) -> a -> a
+    newton f df x0 = until (isCloseEnough 1e-12) nextIteration x0
+      where
+        isCloseEnough eps x = abs (f x)  < eps
+        nextIteration x = x - f x / df x
+
+----------------------------------------------------------------------------
+
 -- Versión 1 (sin until, criterio x2 x1)
 
 heron1 :: Double -> Double -> Double -> Maybe Double
@@ -53,32 +80,6 @@ heron15 a b c
         isCloseEnough (x1, x2) = abs ( x2 - x1 ) < 1e-12
         
         nextStep (_, x2) = (x2, nextIteration x2)
-
--- Versión 2 (until, anidada,  criterio f x)
-
-heron2 :: Double -> Double -> Double -> Maybe Double
-heron2 a b c
-  | not $ isTriangle a b c = Nothing
-  | otherwise              = Just . newton f df $ x0
-  where
-    s = (a+b+c)/2
-    t = s * (s-a) * (s-b) * (s-c)
-    f x = x*x - t
-    df x = 2*x
-    x0 = max 1 $ t/2
-
-    isTriangle :: Double -> Double -> Double -> Bool 
-    isTriangle a b c = and [a > 0, b > 0, c > 0,
-      a + b > c, a + c > b, b + c > a]
-
-    newton :: (Double -> Double) -> (Double -> Double) -> Double -> Double
-    newton f df x0 = until (isCloseEnough 1e-12) nextIteration x0
-      where
-        isCloseEnough :: Double -> Double -> Bool
-        isCloseEnough eps x = abs (f x)  < eps
-        
-        nextIteration :: Double -> Double
-        nextIteration = \x -> x - f x / df x
 
 -- Versión 3 (until, no anidada, criterio f x)
 
