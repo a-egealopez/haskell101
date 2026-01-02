3c222196f98e9801cb6f5d60500b1ed5a1a2a090
Nicram01 <nicram01@ucm.es>
2026-01-02 12:01:45 +0100

    Update vigenere.hs
    Indice de Coincidencia + MutualIndex of Coincidence

--- haskell & prolog/vigenere.hs
+++ haskell & prolog/vigenere.hs
@@ -1,5 +1,5 @@
 import Data.Char (isLetter, toUpper, ord, chr)
-import Data.List (group, sort, nub, elemIndices)
+import Data.List (group, sort, nub, elemIndices, sortOn, maximumBy)
 import Control.Applicative (ZipList(..))
 
 class Monoid g => Group g where
@@ -31,6 +31,7 @@ normalize str = toUpper <$> filter isLetter str
 
 shiftChar :: Char -> Shift -> Char
 shiftChar c (Shift n) = chr (ord 'A' + (ord c - ord 'A' + n) `mod` 26)
+
 
 -------------------- Algoritmo de Vigenère --------------------
 
@@ -64,6 +65,93 @@ possibleLengths str = nub . concatMap factorize $ dists
         (x:_) <- filter ((>1) . length) (group $ sort trigrams)
         let indices = elemIndices x trigrams
         zipWith (-) (drop 1 indices) indices
+
+-------------------- Indice de Coincidencia --------------------
+
+buildSubstrings :: Int -> String -> [String]
+buildSubstrings k msg = [ [ msg !! j | j <- [i, i+k .. length msg - 1] ] | i <- [0 .. k-1]]
+
+frequencies :: String -> [Int]
+frequencies s = map length. group. sort$ s
+
+indexOfCoincidence :: String -> Double
+indexOfCoincidence s
+  | n <= 1    = 0
+  | otherwise = fromIntegral acc / fromIntegral (n * (n - 1))
+  where
+    freqs = frequencies s
+    acc   = sum [ f * (f - 1) | f <- freqs ]
+    n     = sum freqs
+    
+indexOfCoincidenceTest :: Int -> String -> [Double]
+indexOfCoincidenceTest k message = map indexOfCoincidence substrings
+  where
+    substrings = buildSubstrings k (normalize message)
+
+textoCifrado :: String
+textoCifrado = concat
+  [ "UECWKDVLOTTVACKTPVGEZQMDAMRNPDDUXLBUICAMRHOECBHSPQLVIWO"
+  , "FFEAILPNTESMLDRUURIFAEQTTPXADWIAWLACCRPBHSRZIVQWOFROGTT"
+  , "NNXEVIVIBPDTTGAHVIACLAYKGJIEQHGECMESNNOCTHSGGNVWTQHKBPR"
+  , "HMVUOYWLIAFIRIGDBOEBQLIGWARQHNLOISQKEPEIDVXXNETPAXNZGDX"
+  , "WWEYQCTIGONNGJVHSQGEATHSYGSDVVOAQCXLHSPQMDMETRTMDUXTEQQ"
+  , "JMFAEEAAIMEZREGIMUECICBXRVQRSMENNWTXTNSRNBPZHMRVRDYNECG"
+  , "SPMEAVTENXKEQKCTTHSPCMQQHSQGTXMFPBGLWQZRBOEIZHQHGRTOBSG"
+  , "TATTZRNFOSMLEDWESIWDRNAPBFOFHEGIXLFVOGUZLNUSRCRAZGZRTTA"
+  , "YFEHKHMCQNTZLENPUCKBAYCICUBNRPCXIWEYCSIMFPRUTPLXSYCBGCC"
+  , "UYCQJMWIEKGTUBRHVATTLEKVACBXQHGPDZEANNTJZTDRNSDTFEVPDXK"
+  , "TMVNAIQMUQNOHKKOAQMTBKOFSUTUXPRTMXBXNPCLRCEAEOIAWGGVVUS"
+  , "GIOEWLIQFOZKSPVMEBLOHLXDVCYSMGOPJEFCXMRUIGDXNCCRPMLCEWT"
+  , "PZMOQQSAWLPHPTDAWEYJOGQSOAVERCTNQQEAVTUGKLJAXMRTGTIEAFW"
+  , "PTZYIPKESMEAFCGJILSBPLDABNFVRJUXNGQSWIUIGWAAMLDRNNPDXGN"
+  , "PTTGLUHUOBMXSPQNDKBDBTEECLECGRDPTYBVRDATQHKQJMKEFROCLXN"
+  , "FKNSCWANNAHXTRGKCJTTRRUEMQZEAEIPAWEYPAJBBLHUEHMVUNFRPVM"
+  , "EDWEKMHRREOGZBDBROGCGANIUYIBNZQVXTGORUUCUTNBOEIZHEFWNBI"
+  , "GOZGTGWXNRHERBHPHGSIWXNPQMJVBCNEIDVVOAGLPONAPWYPXKEFKOC"
+  , "MQTRTIDZBNQKCPLTTNOBXMGLNRRDNNNQKDPLTLNSUTAXMNPTXMGEZKA"
+  , "EIKAGQ"
+  ]
+
+
+-------------------- Mutual Index Of Coincidence--------------------
+
+
+spanishFreq :: [(Char, Double)]
+spanishFreq =
+  [ ('A',0.1253),('B',0.0142),('C',0.0468),('D',0.0586)
+  , ('E',0.1368),('F',0.0069),('G',0.0101),('H',0.0070)
+  , ('I',0.0625),('J',0.0044),('K',0.0002),('L',0.0497)
+  , ('M',0.0315),('N',0.0671),('O',0.0868),('P',0.0251)
+  , ('Q',0.0088),('R',0.0687),('S',0.0798),('T',0.0463)
+  , ('U',0.0393),('V',0.0090),('W',0.0001),('X',0.0022)
+  , ('Y',0.0090),('Z',0.0052)
+  ]
+
+relativeFreq :: Char -> String -> Double
+relativeFreq c s = fromIntegral (length (filter (== c) s)) / fromIntegral (length s)
+
+mutualIndex :: String -> Double
+mutualIndex s = sum [ p * relativeFreq c s | (c,p) <- spanishFreq ]
+
+micForShift :: String -> Int -> Double
+micForShift s b = mutualIndex (map (`shiftChar` Shift b) s)
+
+bestShift :: String -> Int
+bestShift s = snd $ maximumBy cmp results
+  where
+    results = [ (micForShift s b, b) | b <- [0..25] ]
+    cmp (x,_) (y,_) = compare x y
+
+shiftToKeyChar :: Int -> Char
+shiftToKeyChar b = chr (ord 'A' + k)
+  where
+    k = (26 - b) `mod` 26
+
+
+mutualIndexTest :: Int -> String -> String
+mutualIndexTest k msg = map (shiftToKeyChar . bestShift) substrings
+  where
+    substrings = buildSubstrings k (normalize msg)
 
 {--
 ------------------- EXPLICACIÓN DE dists --------------------
